27c1f9bcbc0340f8dd7f8fdbe9315228092fc2dd
Robert Kashyap <orionalchemist@gmail.com>
2024-01-22 11:15:31 +0530

    Spring boot project initiated with maven dependencies

---
+++ src/test/java/com/spbt/traditonaltodoapp/TraditonalTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spbt.traditonaltodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraditonalTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
